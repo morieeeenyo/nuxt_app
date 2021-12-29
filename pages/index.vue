5da5f3902def9cb1f4925363322d575b926a8b9b
togo-mentor <uint2435@gmail.com>
2021-12-29 17:36:49 +0900

    main.scssの読み込み

--- pages/index.vue
+++ pages/index.vue
@@ -40,12 +40,8 @@
           {{ color }}
         </v-btn>
       </v-card-text>
-      <v-card-title>
-        VuetifyカスタムCSSの検証
-      </v-card-title>
-      <v-card-text>
-        ipad（768px）とmobile（426px）で表示・非表示
-      </v-card-text>
+      <v-card-title> VuetifyカスタムCSSの検証 </v-card-title>
+      <v-card-text> ipad（768px）とmobile（426px）で表示・非表示 </v-card-text>
       <v-card-text>
         <v-card
           v-for="(cls, i) in customClass"
@@ -75,11 +71,19 @@ export default {
     return {
       colors: ["primary", "info", "success", "warning", "error", "background"],
       customClass: [
-        { name: 'hidden-ipad-and-down', color: 'error', des: 'ipad未満で隠す' },
-        { name: 'hidden-ipad-and-up', color: 'info', des: 'ipad以上で隠す' },
-        { name: 'hidden-mobile-and-down', color: 'success', des: 'mobile未満で隠す' },
-        { name: 'hidden-mobile-and-up', color: 'warning', des: 'mobile以上で隠す' }
-      ]
+        { name: "hidden-ipad-and-down", color: "error", des: "ipad未満で隠す" },
+        { name: "hidden-ipad-and-up", color: "info", des: "ipad以上で隠す" },
+        {
+          name: "hidden-mobile-and-down",
+          color: "success",
+          des: "mobile未満で隠す",
+        },
+        {
+          name: "hidden-mobile-and-up",
+          color: "warning",
+          des: "mobile以上で隠す",
+        },
+      ],
     }
   },
   computed: {
