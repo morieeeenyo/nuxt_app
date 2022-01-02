bfdb7cb9b746e055def0eca6a3384f8397b1e192
togo-mentor <uint2435@gmail.com>
2022-01-02 13:17:29 +0900

    フッターを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <template>
   <v-app>
     <!-- isプロパティを使わない場合はimportなしで使える -->
-    <home-app-bar
-      :menus="menus"
-      :img-height="imgHeight"
-    />
+    <home-app-bar :menus="menus" :img-height="imgHeight" />
     <!-- gradient→グラデーション -->
     <!-- lorempicsumから画像を取得 -->
     <v-img
@@ -19,35 +16,18 @@
         justify="center"
         :style="{ height: `${imgHeight}px` }"
       >
-        <v-col
-          cols="12"
-          class="text-center"
-        >
-          <h1 class="display-1 mb-4">
-            未来を作ろう。ワクワクしよう。
-          </h1>
-          <h4
-            class="subheading"
-            :style="{ letterSpacing: '5px' }"
-          >
+        <v-col cols="12" class="text-center">
+          <h1 class="display-1 mb-4">未来を作ろう。ワクワクしよう。</h1>
+          <h4 class="subheading" :style="{ letterSpacing: '5px' }">
             中小企業に特化した事業計画策定ツール
           </h4>
         </v-col>
       </v-row>
     </v-img>
     <v-sheet>
-      <v-container
-        fluid
-        :style="{ maxWidth: '1280px' }"
-      >
-        <v-row
-          v-for="(menu, i) in menus"
-          :key="`menu-${i}`"
-        >
-          <v-col
-          :id="menu.title"
-          cols="12"
-            >
+      <v-container fluid :style="{ maxWidth: '1280px' }">
+        <v-row v-for="(menu, i) in menus" :key="`menu-${i}`">
+          <v-col :id="menu.title" cols="12">
             <v-card flat>
               <v-card-title class="justify-center display-1">
                 {{ $t(`menus.${menu.title}`) }}
@@ -66,16 +46,17 @@
         </v-row>
       </v-container>
     </v-sheet>
+    <app-footer/>
   </v-app>
 </template>
 
 <script>
 // コンポーネントのimport。isから呼び出す場合はimport必須
-import HomeAbout from '~/components/home/HomeAbout'
-import HomeProducts from '~/components/home/HomeProducts'
-import HomePrice from '~/components/home/HomePrice'
-import HomeContact from '~/components/home/HomeContact'
-import HomeCompany from '~/components/home/HomeCompany'
+import HomeAbout from "~/components/home/HomeAbout"
+import HomeProducts from "~/components/home/HomeProducts"
+import HomePrice from "~/components/home/HomePrice"
+import HomeContact from "~/components/home/HomeContact"
+import HomeCompany from "~/components/home/HomeCompany"
 
 export default {
   components: {
@@ -83,19 +64,23 @@ export default {
     HomeProducts,
     HomePrice,
     HomeContact,
-    HomeCompany
+    HomeCompany,
   },
-  data () {
+  data() {
     return {
       imgHeight: 500, // 画像の高さ
       menus: [
-        { title: 'about', subtitle: 'このサイトはブログ"独学プログラマ"で公開されているチュートリアルのデモアプリケーションです' },
-        { title: 'products', subtitle: '他にはない優れた機能の数々' },
-        { title: 'price', subtitle: '会社の成長に合わせた3つのプラン' },
-        { title: 'contact', subtitle: 'お気軽にご連絡を' },
-        { title: 'company', subtitle: '私たちの会社' }
-      ]
+        {
+          title: "about",
+          subtitle:
+            'このサイトはブログ"独学プログラマ"で公開されているチュートリアルのデモアプリケーションです',
+        },
+        { title: "products", subtitle: "他にはない優れた機能の数々" },
+        { title: "price", subtitle: "会社の成長に合わせた3つのプラン" },
+        { title: "contact", subtitle: "お気軽にご連絡を" },
+        { title: "company", subtitle: "私たちの会社" },
+      ],
     }
-  }
+  },
 }
 </script>
